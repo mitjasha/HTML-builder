daf0e7ce9245cf7e1399025c67b280df0088842f
mitjasha <romantus_@mail.ru>
2021-11-04 17:44:45 +0300

    feat: add task 03 solution

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,9 +2,13 @@ const fs = require('fs');
 const path = require('path');
 
 fs.readdir(path.join(__dirname, 'secret-folder'),{withFileTypes: true}, (err, files) => {
+  if (err) throw err;
+
   files.forEach((file) => {
     fs.stat(path.join(__dirname, 'secret-folder', file.name), (err, stats) => {
-      process.stdout.write(`${file.name.split('.')[0]} - ${path.extname(path.join(__dirname, 'secret-folder', file.name)).slice(1)} - ${(stats.size).toFixed(0)} bytes\n`);
+      if (err) throw err;
+
+      process.stdout.write(`${file.name.split('.')[0]} - ${file.name.split('.')[1]} - ${(stats.size).toFixed(0)} bytes\n`);
     });
   });
 });
