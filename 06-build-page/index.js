caeb4096f9f3ff6a1830c698ba3462a2b2b13a37
mitjasha <romantus_@mail.ru>
2021-11-08 15:02:41 +0300

    feat: task 06 add copy assets and create style functions

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,7 +1,53 @@
 const fs = require('fs');
 const path = require('path');
-const template = fs.createReadStream(path.join(__dirname, 'template.html'));
+// const template = fs.createReadStream(path.join(__dirname, 'template.html'));
 
 fs.mkdir(path.join(__dirname, 'project-dist'), { recursive: true }, () => {});
-console.log(template);
+// console.log(template);
+
+// ==========COPY ASSETS=========
+const copy = async (src, target) => {
+  await fs.mkdir(target, {recursive: true}, () => {});
+  fs.readdir(src, {withFileTypes: true}, (err, files) => {
+    files.forEach((file) => {
+      let srcPath = path.join(src, file.name);
+      let targetPath = path.join(target, file.name);
+      if(file.isDirectory()) {
+        copy(srcPath, targetPath);
+      } else {
+        fs.copyFile(srcPath, targetPath,() => {});
+      }
+    });
+  });
+};
+
+const src = path.join(__dirname, 'assets');
+const target = path.join(__dirname, 'project-dist');
+
+const copyFolder = async (src, target) => {
+  await fs.mkdir(target, {recursive: true}, () => {});
+  const newFolder = path.join(target, 'assets');
+  await copy(src, newFolder);
+};
+
+copyFolder(src, target);
+
+// ===========STYLES========
+const styles = fs.createWriteStream('./06-build-page/project-dist/style.css', 'utf-8');
+
+fs.readdir(path.join(__dirname, 'styles'),{ withFileTypes: true }, (err, files) => {
+  if (err) throw err;
+    
+  files.forEach(file =>{
+    fs.stat(path.join(__dirname, 'styles', file.name), (err, stats) => {
+      if (err) throw err;
+        
+      if (stats.isFile() && path.extname(file.name) === '.css'){
+        const input = fs.createReadStream(path.join(__dirname, 'styles', file.name));
+          
+        input.on('data', partData => styles.write(partData + '\n'));
+      }
+    });
+  });
+});
 
